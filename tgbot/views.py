cb141923e155e2153abeac31a40683d4c1c74e5c
Timur2311 <zayniddinov.programming@gmail.com>
2022-10-17 17:26:54 +0500

    webhook settings changed

--- tgbot/views.py
+++ tgbot/views.py
@@ -2,36 +2,15 @@ import json
 import logging
 from django.views import View
 from django.http import JsonResponse
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
 
 from core.settings import DEBUG
 from tgbot.dispatcher import process_telegram_event
-
-from tgbot.forms import QuestionForm
-from utils.saving_data import saving_data
 
 logger = logging.getLogger(__name__)
 
 
 def index(request):
-    return render(request, 'index.html')
-
-def add_question(request):
-    if request.method == "POST":
-        # print(f"{request.FILES}")
-        form = QuestionForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            
-        saving_data()
-            
-            # <process form cleaned data>
-    else:
-        form = QuestionForm()
-
-    return render(request, 'form_template.html', {'form': form})
-    
+    return JsonResponse({"error": "sup hacker"})
 
 
 class TelegramBotWebhookView(View):
@@ -44,7 +23,6 @@ class TelegramBotWebhookView(View):
             # Process Telegram event in Celery worker (async)
             # Don't forget to run it and & Redis (message broker for Celery)! 
             # Read Procfile for details
-            # You can run all of these services via docker-compose.yml
             process_telegram_event.delay(json.loads(request.body))
 
         # TODO: there is a great trick to send action in webhook response
