c5aa85ea7dc6a7a7b272310e1cfda7172fdd7004
Timur2311 <zayniddinov.programming@gmail.com>
2022-10-17 17:39:44 +0500

    webhook view

--- tgbot/views.py
+++ tgbot/views.py
@@ -2,15 +2,36 @@ import json
 import logging
 from django.views import View
 from django.http import JsonResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 from core.settings import DEBUG
 from tgbot.dispatcher import process_telegram_event
+
+from tgbot.forms import QuestionForm
+from utils.saving_data import saving_data
 
 logger = logging.getLogger(__name__)
 
 
 def index(request):
-    return JsonResponse({"error": "sup hacker"})
+    return render(request, 'index.html')
+
+def add_question(request):
+    if request.method == "POST":
+        # print(f"{request.FILES}")
+        form = QuestionForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            
+        saving_data()
+            
+            # <process form cleaned data>
+    else:
+        form = QuestionForm()
+
+    return render(request, 'form_template.html', {'form': form})
+    
 
 
 class TelegramBotWebhookView(View):
