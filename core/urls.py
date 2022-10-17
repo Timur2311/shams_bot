fc97a4f0000a621ada9463fc7f63396fda22e237
Timur2311 <zayniddinov.programming@gmail.com>
2022-10-17 07:01:59 +0500

    staticfiles, templates

--- core/urls.py
+++ core/urls.py
@@ -26,3 +26,5 @@ urlpatterns = [
     path('', include('tgbot.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
